77d80a53c5865931802f79a96220e296931adc6e
joao-zanetti <57362590+joao-zanetti@users.noreply.github.com>
2019-12-27 02:01:03 -0300

    Add files via upload

---
+++ Poster.py
@@ -0,0 +1,18 @@
+import json
+import requests 
+import os
+
+class Poster():
+
+    #Cria o arquivo resposta.json com o dicionario de respostas
+    #Efetua requisição post para a API RaccoonPoster
+    #Recebe a resposta da requisição post
+    @staticmethod
+    def post(resposta):
+        #Escreve a resposta dos exercícios no arquivo resposta.json em json
+        with open('resposta.json','w') as f:
+            json.dump(resposta,f,indent=4)
+            
+        os.system("curl -H \"Content-Type: application/json\" --data @resposta.json https://us-central1-psel-clt-ti-junho-2019.cloudfunctions.net/psel_2019_post")
+        
+        return 
